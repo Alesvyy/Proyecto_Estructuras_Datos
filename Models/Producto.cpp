f887cd9a9dd2b185d8f4570d2a95b7602036c47c
ramz8426 <bramirezc16@gmail.com>
2024-10-14 20:54:23 -0600

    Creación de Producto

---
+++ Models/Producto.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Bryan Ramirez Campos on 14/10/24.
+//
+
+#include "Producto.h"
---
+++ Models/Producto.h
@@ -0,0 +1,19 @@
+//
+// Created by Bryan Ramirez Campos on 14/10/24.
+//
+
+#ifndef PRODUCTO_H
+#define PRODUCTO_H
+
+
+
+class Producto {
+
+
+
+
+};
+
+
+
+#endif //PRODUCTO_H
